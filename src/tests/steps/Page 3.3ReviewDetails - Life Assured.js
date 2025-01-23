30d33ded9bab98cc87d6f79451054c3ab0d0be4e
thilagavathirajan19 <thilagavathirajan19@outlook.com>
2025-01-23 12:34:31 +0530

    step files added

---
+++ src/tests/steps/Page 3.3ReviewDetails - Life Assured.js
@@ -0,0 +1,13 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+Given('user able to view the values in life assured tab', async function () {
+    
+  });
+  When('user able to fill the values in all mandatory fields', async function () {
+    
+  });
+  When('user click the nex button', async function () {
+    
+  });
+  Then('user should validate the logo icon has changed to green colour', async function () {
+    
+  });
---
+++ src/tests/steps/Page 3.4ReviewDetails - UnderwritingQuestions.js
@@ -0,0 +1,26 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+Given('user able to reach the underwriting tab', async function () {
+    
+  });
+
+When('user able to fill the height and weight fields', async function () {
+    
+  });
+
+When('user click the calculate button', async function () {
+   
+  });
+When('user able to see the newly generated underwriting Questions', async function () {
+    
+  });
+
+When('user complete all the underwriting questions appropratiely', async function () {
+    
+  });
+When('user click the nex button', async function () {
+    
+  });
+
+Then('user able to see the green logo in underwriting tab', async function () {
+    
+  });
