834c0aae77a4ce1f304fe1ccda51a1b4a14b76c4
Mageshpoopathi <mageshpoopathi208@gmail.com>
2025-01-23 11:54:42 +0530

    Created New Step definition files

---
+++ src/tests/steps/Page 4.1ReviewDetails - ROP.js
@@ -0,0 +1,18 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+Given('user able to view ques', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+  When('User selects No option', async function () {
+    // Write code here that turns the phrase above into concrete actions     
+    return 'pending';
+  });
+  When('user clicks next', async function () {
+    // Write code here that turns the phrase above into concrete actions     
+    return 'pending';
+  });
+  Then('user validate the payer details text', async function () {
+    // Write code here that turns the phrase above into concrete actions     
+    return 'pending';
+  });
---
+++ src/tests/steps/Page 5.1Declaration - Client Acknowledgement.js
@@ -0,0 +1,33 @@
+const { Given, When, Then } = require('@cucumber/cucumber');
+
+  Given('user able to view terms', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+
+  When('user selects terms option', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+
+  When('user selects e-documents option', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+  When('user enter contact details', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+
+  Then('user validate the text declaration of politically', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
