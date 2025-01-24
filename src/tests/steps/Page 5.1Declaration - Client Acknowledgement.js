f945a82764b12059eac768b7c9e0765446886203
kedar2325 <kedarnathmob@gmail.com>
2025-01-24 19:53:59 +0530

    Added Page - 5.1

--- src/tests/steps/Page 5.1Declaration - Client Acknowledgement.js
+++ src/tests/steps/Page 5.1Declaration - Client Acknowledgement.js
@@ -1,6 +1,14 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+const { setDefaultTimeout } = require('@cucumber/cucumber');
+setDefaultTimeout(15000); 
+require('dotenv').config();
 
+const { ClientAcknowledgement } = require('../Pages/Page 5.1Declaration - Client Acknowledgement');
+const { pageObject } = require('../Hooks/PageObjects');
+
+let ClientAcknowledgement;
   Given('user able to view terms', async function () {
+    ClientAcknowledgement=new ClientAcknowledgement(pageObject.page);
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
   });
