93d7f1e872e63fde3a3b5ca8ef11a56c1d32c1c8
MadhuMohan570 <madhumitha.mohanraj@expleogroup.com>
2025-01-24 12:58:50 +0530

    steps updated in 4.1 4.2 4.3

--- src/tests/Pages/Page 4.2ReviewDetails - PayorDetails.js
+++ src/tests/Pages/Page 4.2ReviewDetails - PayorDetails.js
@@ -1,17 +1,23 @@
+const { expect } = require('@playwright/test');
 const { clickAndSendkeys, launchURL, sleep } = require('../Helper/Action');
 const { pageObject } = require('../Hooks/PageObjects');
 require('dotenv').config();
 
 const PageLocators={
+    verifyInitialPremiumPage: "//p[contains(text(),'Initial Premium Payment')]",
     payerDetails: "(//div[@data-testid='radio-items'])[2]",
     sourceOfWealth : "(//img[@id='tick_icon'])[1]",
     sourceOfFunds: "(//img[@id='tick_icon'])[7]",
-    nextBtn: "//button[contains(text(),'Next')]"
+    nextBtn: "//button[contains(text(),'Next')]",
+    verifyPayerDetails: "//span[contains(text(),'Are you paying for this policy?')]"
     
 }
 
 class PayorDetails{
-    async payer(){
+    async verifyPage(){
+        await assertText(PageLocators.verifyPayerDetails, "Are you paying for this policy?"); 
+    }
+    async payerYes(){
         await Click(PageLocators.payerDetails);
     }
     async sourceWealth(){
@@ -22,6 +28,9 @@ class PayorDetails{
     }
     async button(){
         await Click(PageLocators.nextBtn);
+    }
+    async verifyInitialPremiumPage(){ 
+        await assertText(PageLocators.verifyInitialPremiumPage, "Initial Premium Payment");
     }
 }
 module.exports={PayorDetails}
--- src/tests/steps/Page 4.3ReviewDetails-InitiPremiumPymt.js
+++ src/tests/steps/Page 4.3ReviewDetails-InitiPremiumPymt.js
@@ -1,34 +1,32 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
+const { setDefaultTimeout } = require('@cucumber/cucumber');
+setDefaultTimeout(15000); 
+require('dotenv').config();
 
-         Given('user able to view payment method', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+//login import
+const { ReviewDetailsInitialPremiumPayment } = require('../Pages/Page 4.3ReviewDetails-InitiPremiumPymt');
+const { pageObject } = require('../Hooks/PageObjects');
 
+let InitialPremiumPymt;
 
+Given('user able to view payment method', async function () {
+  InitialPremiumPymt=new ReviewDetailsInitialPremiumPayment(pageObject.page);
+  await InitialPremiumPymt.verifyInitialPremiumPage();
+});
 
-         When('user selects payment method', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+When('user selects payment method', async function () {
+  await InitialPremiumPymt.InitialPayment();
+  await InitialPremiumPymt.EnterCardDetails();
+});
 
+When('user selects subsequent premium payment', async function () {
+  await InitialPremiumPymt.ClickCash();
+});
 
+When('user click on Next', async function () {
+  await InitialPremiumPymt.ClickNextButton();
+});
 
-         When('user selects subsequent premium payment', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-
-
-         When('user click on Next', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
-
-
-
-         Then('user validate the on your terms text', async function () {
-           // Write code here that turns the phrase above into concrete actions
-           return 'pending';
-         });
+Then('user validate the on your terms text', async function () {
+  await InitialPremiumPymt.verifyDeclarationPage();
+});
