91aafedb2db8199cdcac8302b0cc69afdb6bda38
kedar2325 <kedarnathmob@gmail.com>
2025-01-29 20:51:58 +0530

    Updated 3.4

--- src/tests/Pages/Page 3.4ReviewDetails - UnderwritingQuestions.js
+++ src/tests/Pages/Page 3.4ReviewDetails - UnderwritingQuestions.js
@@ -1,3 +1,4 @@
+const { Console } = require('console');
 const { clickAndSendkeys, sleep, toClick, assertText, Click, sendkeys, isVisible } = require('../Helper/Action');
 const { pageObject } = require('../Hooks/PageObjects');
 require('dotenv').config();
@@ -11,7 +12,8 @@ const PageLocators={
     underwritingNotReq:"//div[contains(text(),'This section is not required for this application')]",
     //
     underwitingQuestion:"span[contains(text(),'In the last 12 months preceding the date of this application, have you been residing in Singapore for more than 183 days?')]/parent::p/parent::div/parent::div/parent::div//following-sibling::div/div/p[text()='No']",
-    underwitingQuestion:"span[contains(text(),'In the last 12 months preceding the date of this application, have you been residing in Singapore for more than 183 days?')]/parent::p/parent::div/parent::div/parent::div//following-sibling::div/div/p[text()='No']"
+    underwitingQuestion:"span[contains(text(),'In the last 12 months preceding the date of this application, have you been residing in Singapore for more than 183 days?')]/parent::p/parent::div/parent::div/parent::div//following-sibling::div/div/p[text()='No']",
+    next_btn: "//button[text()='Next']",
     //Questions
     // 1.In the last 12 months preceding the date of this application, have you been residing in Singapore
     // 2.In the next 12 months, do you plan to spend more than 90 days outside of your current country
@@ -43,28 +45,27 @@ class underwritingQuestions{
         await sleep(8000);
     }
     async verifyUnderwriting(){
-        let path = ""
-        const exists = await isVisible(PageLocators.underwritingNotReq)
-        if(exists){
-            console.log("Underwriting not req")
-        }else{
-            let ListofQA=process.env.Rider.split('|');
+        let path=""
+            let ListofQA=process.env.UnderwritingQuestions.split('|');
             for (let CombineQA of ListofQA) {
                 let Answer = CombineQA.split(',');
                 const locatorText = Answer[0];
                 const locatorValue = Answer[1];
-                path=`"span[contains(text(),"'${locatorText},')]/parent::p/parent::div/parent::div/parent::div//following-sibling::div/div/p[text()='${locatorValue}']"`;
+                console.log(locatorText);
+                console.log(locatorValue);
+                path=`//span[contains(text(),'${locatorText}')]/parent::p/parent::div/parent::div/parent::div//following-sibling::div/div/p[text()='${locatorValue}']`;
                 // for (let QALocator of Answer){ 
                 //     path=path.concat(path,QALocator);
                 // }
-                toClick(path);
+                await toClick(path);
+                console.log(path);
                 await sleep(2000);
                 path="";
             }
-        }
+            console.log("loop out")
     }
     async selectUnderwritingQA(){
-
+        console.log("to check if called")
     }
     // async VerifyUnderwriting() {
     //     await toClick("(//p[text()='No'])[1]");
@@ -84,5 +85,10 @@ class underwritingQuestions{
     //         await toClick(locator);
     //     }
     // }
+    async clickNextButton(){
+            console.log("to click next button")
+            await Click(PageLocators.next_btn);
+            console.log("clicked next button")
+        }
 }
 module.exports={underwritingQuestions}
--- src/tests/features/Page 3.4ReviewDetails - UnderwritingQuestions.feature
+++ src/tests/features/Page 3.4ReviewDetails - UnderwritingQuestions.feature
@@ -4,7 +4,5 @@ Feature:Review Details - Underwriting Questions
   Given user able to reach the underwriting tab
   When user able to fill the height and weight fields
   And user click the calculate button
-  And user able to see the newly generated underwriting Questions
-  And user complete all the underwriting questions appropratiely
-  Then user click the next button
-  
+  And user answer underwriting Questions
+  Then user click the next button on underwriting page
