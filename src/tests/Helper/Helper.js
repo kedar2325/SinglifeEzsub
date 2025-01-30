de193fa8fe585642fbc3a6a06d8d62fa19b10539
kedar2325 <kedarnathmob@gmail.com>
2025-01-30 20:13:53 +0530

    Updated Package json

--- src/tests/Helper/Helper.js
+++ src/tests/Helper/Helper.js
@@ -80,7 +80,7 @@ async function Signature(Webelement){
         await mouseUp();
 }
 function excelValue(){
-    const excelData=readExcelData("C:/Users/12129.ARDIANET.000/Desktop/SinglifeEzsub/data.xlsx","Sheet1")
+    const excelData=readExcelData("C:/Users/7821/Documents/Ezsubautomation/SinglifeEzsub/data.xlsx","Sheet1")
     return excelData;
 }
 
